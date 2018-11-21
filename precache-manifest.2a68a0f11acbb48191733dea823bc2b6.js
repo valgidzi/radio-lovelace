03dba4cb91122c678f4a1e3c65795c8e5978beb2
Dan Roberts <dan@adadevelopersacademy.org>
2018-11-20 22:42:41 -0800

    Updates

---
+++ precache-manifest.2a68a0f11acbb48191733dea823bc2b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "acfda44954b9ecc35a14",
+    "url": "/radio-lovelace/static/css/main.60734c2f.chunk.css"
+  },
+  {
+    "revision": "acfda44954b9ecc35a14",
+    "url": "/radio-lovelace/static/js/main.acfda449.chunk.js"
+  },
+  {
+    "revision": "4c6b97bfd7bb8f9f542d",
+    "url": "/radio-lovelace/static/js/1.4c6b97bf.chunk.js"
+  },
+  {
+    "revision": "65f0cdff51ec13e12d70",
+    "url": "/radio-lovelace/static/js/runtime~main.65f0cdff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/radio-lovelace/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "005e44b788834bd641cfb67b465b14bf",
+    "url": "/radio-lovelace/index.html"
+  }
+];
