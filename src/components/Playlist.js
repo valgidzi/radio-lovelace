aca9e7add230656698146619a1c8939e0a87fb5f
Dan Roberts <dan@adadevelopersacademy.org>
2018-11-25 13:38:59 -0800

    Add Playlist component with no event handling

---
+++ src/components/Playlist.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './styles/Playlist.css';
+
+import Track from './Track';
+
+const calculatePlayTime = (tracks) => {
+  let minutes = 0;
+  let seconds = 0;
+  tracks.forEach((track) => {
+    const times = track.playtime.split(':');
+    minutes += parseInt(times[0]);
+    seconds += parseInt(times[1]);
+  });
+
+  minutes += Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+
+  seconds %= 60;
+  minutes %= 60;
+
+  seconds = ("" + seconds).padStart(2, "0");
+  minutes = ("" + minutes).padStart(2, "0");
+
+  return `${hours}:${minutes}:${seconds}`;
+}
+
+const Playlist = (props) => {
+  const tracks = props.tracks;
+  const trackCount = tracks.length;
+  const playtime = calculatePlayTime(tracks);
+  const trackElements = tracks.map((track, i) => {
+    // We use "spread syntax" here to pass in all the properties of 
+    // the variable 'track' as props. Go look it up!
+    return (
+      <Track
+        key={i}
+        {...track}
+      />
+    );
+  });
+
+  return (
+    <div className="playlist">
+      <h2>{props.side} Playlist</h2>
+      <p>
+        {trackCount} tracks - {playtime}
+      </p>
+      <ul className="playlist--track-list">
+        {trackElements}
+      </ul>
+    </div>
+  );
+}
+
+Playlist.propTypes = {
+  tracks: PropTypes.array,
+  side: PropTypes.string,
+}
+
+export default Playlist;
